4ee3708c253196277506986bf29b53ab27294831
Frankie <1426203851@qq.com>
2018-04-16 03:29:33 +0800

    优化注册路程，新增一些过渡动画

--- src/components/login/LoginNext.vue
+++ src/components/login/LoginNext.vue
@@ -14,8 +14,8 @@
 				<mu-raised-button label="完成" @click="haveDone"/>
 			</mu-col>
 		</div>
-		<mu-dialog :open="dialog" title="Alert Dialog">
-			只有点击确定按钮才可以关闭对话框
+		<mu-dialog :open="dialog" title="错误提示">
+			{{errorTips}}
 			<mu-flat-button label="确定" slot="actions" primary @click="closeDialog"/>
 		</mu-dialog>
 	</div>
@@ -38,8 +38,8 @@
 				value_password: '',
 				errorText_password: '',
 				info: '',
-				dialog: false
-				
+				dialog: false,
+				errorTips: '',
 			}
 		},
 		created () {
@@ -50,24 +50,24 @@
 			// 当直接在地址栏输入 /login/2 时，重定向返回登录
 			if (this.$route.params.username == '' || this.$route.params.username == null || this.$route.params.username == undefined ) {
 				this.info = '请先选择账号！'
-				// const toast = Toast.loading({
-				// 	mask: true,
-				// 	duration: 0,	// 持续展示 toast
-				// 	forbidClick: true,	// 禁用背景点击
-				// 	message: '出现错误…'
-				// });
-				// let second = 1;
-				// const timer = setInterval(() => {
-				// 	second--;
-				// 	if (second == 0) {
-				// 		clearInterval(timer);
-				// 		Toast.clear();
-				// 		let redirect = decodeURIComponent(this.$route.query.redirect || '/login');
-				// 		this.$router.push({path: redirect})
-				// 	}
-				// }, 1000);
+				const toast = Toast.loading({
+					mask: true,
+					duration: 0,	// 持续展示 toast
+					forbidClick: true,	// 禁用背景点击
+					message: '出现错误…'
+				});
+				let second = 1;
+				const timer = setInterval(() => {
+					second--;
+					if (second == 0) {
+						clearInterval(timer);
+						Toast.clear();
+						let redirect = decodeURIComponent(this.$route.query.redirect || '/login');
+						this.$router.push({path: redirect})
+					}
+				}, 1000);
 			} else {
-				this.info = "使用手机号码 " + this.$route.params.username + " 登录"
+				this.info = "使用账号 " + this.$route.params.username + " 登录"
 			}
 		},
 		methods: {
@@ -75,40 +75,57 @@
 				this.$router.push({name: 'Home'});
 			},
 			haveDone () {
-				this.errorText_password = checkFormat.checkPassword(this.value_password)
-				let that = this;
-				if (this.errorText_password == '') {
-					this.$axios.post("/auth/login",
+				let _this = this;
+				_this.errorText_password = checkFormat.checkPassword(_this.value_password)
+				if (_this.errorText_password == '') {
+					_this.$axios.post("/api/auth/login",
 						{
-							username: "ppp", 
-							password: "a123456"
+							// username: "ppp", 
+							// password: "a123456"
 							// username: "admin", 
 							// password: "admin"
-							// username: that.$route.params.username,
-							// password: that.value_password
+							username: _this.$route.params.username,
+							password: _this.value_password
 						},
 						{
 							headers: {'Content-Type': 'application/json'}
 						}
 					)
-					.then(function (response) {
+					.then( (response) => {
 						console.log(response);
-						// console.log('返回的token：' + response.data.token);
 						if (response.status == 200) {
-								store.commit(types.LOGIN, response.data.token)
-								that.$store.commit(that.$types.LOGIN, response.data.token)
-								// that.$router.push({path: '/', name: 'Home', params:{ userId: response.status }})
-						} else {
-							alert("登录失败")
+								_this.$store.dispatch('login', response.data.data.token)
+								const toast = Toast.loading({
+									duration: 0,
+									forbidClick: true,
+									message: '登录成功…'
+								});
+									let second = 2;
+									const timer = setInterval(() => {
+										second--;
+										if (second == 1) {
+											toast.message = '前往首页…';
+										} else {
+											clearInterval(timer);
+											Toast.clear();
+											_this.$router.push({name: 'Home'})
+										}
+								}, 1000);
 						}
 					})
-					.catch(function (error) {
+					.catch((error) => {
 						console.log(error);
-						// alert(error)
-						// that.dialog = true;
+						if (error.status == 401) {
+							_this.errorTips = '密码错误，请重新输入！';
+							_this.dialog = true;
+						}
+						if (error.status == 500) {
+							_this.errorTips = '服务器内部错误（' + error.status +'）';
+							_this.dialog = true;
+						}
 					})
 				} else {
-					this.errorText_password = checkFormat.checkPassword(this.value_password)
+					_this.errorText_password = checkFormat.checkPassword(_this.value_password)
 				}
 				
 			},
