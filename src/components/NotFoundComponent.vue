b1035adc0fcabb82c401fbdd6b9a607fb9ddb318
Frankie <1426203851@qq.com>
2018-04-18 13:25:43 +0800

    增加NotFoundComponent.vue组件

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,29 @@
+<template>
+	<div style="height: 300px; ">
+		<h1>Page Not Found，404！</h1>
+		<mu-raised-button label="返回首页" class="demo-raised-button" @click="goHome"/>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+			goHome () {
+				this.$router.push({name: 'Home'})
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	* {
+		text-align: center;
+	}
+</style>
+
+
