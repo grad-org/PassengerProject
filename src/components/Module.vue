163fe65d1520662bcada8c904a126b7f9c6faa35
Frankie <1426203851@qq.com>
2018-04-21 23:54:09 +0800

    Vue组件模板页，快速复制

---
+++ src/components/Module.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		},
+		created () {
+
+		},
+		mounted () {
+
+		},
+		methods: {
+
+		},
+		computed: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
+
+
