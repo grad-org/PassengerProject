1b67a6c7a5c3cb493a45ee2a308f351db3aaf114
Frankie <1426203851@qq.com>
2018-04-28 01:02:47 +0800

    新增自动获取位置，移动地图获取位置

--- src/components/map/BaiduMap.vue
+++ src/components/map/BaiduMap.vue
@@ -1,6 +1,8 @@
 <template>
 	<!-- ready是组件加载完之后才能执行的代码 -->
 	<baidu-map class="map" :center="center" :zoom="zoom" @ready="handler" @load="loadding" :scroll-wheel-zoom="true"
+		@moveend="moveend"
+		@movestart="movestart"
 		:mapStyle="{styleJson: styleJson}">
 		<!-- 其中bm-geolocation中的locationIcon属性要加上“:”，否则会报错！ -->
 		<bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="false" :autoLocation="false"
@@ -11,20 +13,21 @@
 		<bm-marker :position="autoLocationPoint"
 			:icon="{url: require('../../svg/location.svg'), size: {width: 18, height: 18}}" v-if="initLocation">
 		</bm-marker>
-		<bm-marker :position="enableSelectPoint"
+		<!-- <bm-marker :position="enableSelectPoint"
 			:icon="{url: require('../../svg/enableselect.svg'), size: {width: 14, height: 14}}">
-			<!-- <bm-label content="广东海洋大学" :labelStyle="{color: '#00bcd4', fontSize : '12px'}" :offset="{width: 15, height: 0}"/> -->
 			<my-label :position="{lng: 110.307236, lat: 21.157355}" :text="selectAddress" :active="active"
 				@mouseover.native="active = true" @mouseleave.native="active = false">
 			</my-label>
 		</bm-marker>
 		<bm-marker :position="outsetPoint" 
 			:icon="{url: require('../../svg/outset.svg'), size: {width: 20, height: 20}}">
-			<!-- <my-label :position="outsetPoint" text="起点" ></my-label> -->
 		</bm-marker>
 		<bm-marker :position="destinationPoint" 
 			:icon="{url: require('../../svg/destination.svg'), size: {width: 20, height: 20}}">
-			<!-- <my-label :position="outsetPoint" text="终点" ></my-label> -->
+		</bm-marker> -->
+		<bm-marker :position="centerIconPoint" 
+			:icon="{url: require('../../svg/centericon.svg'), size: {width: 20, height: 31}}"
+			:offset="{width: 0, height: -14}">
 		</bm-marker>
 	</baidu-map>
 	
@@ -55,7 +58,10 @@
 				// 建立连接用
 				stompClient: null,
 
+				map: null,	// 指定map对象
+				BMap: null,	// 指定BMap对象
 				autoLocationPoint: {lng: 0, lat: 0},
+				centerIconPoint: {lng: 0, lat: 0},
 				initLocation: false,
 			}
 		},
@@ -71,6 +77,9 @@
 				console.log('BMap',BMap);
 				console.log('map',map);
 				let _this = this;	// 设置一个临时变量指向vue实例，因为在百度地图回调里使用this，指向的不是vue实例；
+				let _map = map;
+				_this.map = map;	// 创建map对象，然后赋给map属性，以方便在别的方法使用，下同
+				_this.BMap = BMap;
 				let rs = this.$route.params.selectStatus;	// 当选择城市，返回首页是不进行定位
 				let rs1 = this.$route.params.searchStatus;	// 当用户搜索起点或终点时，不对其自动获取当前位置
 				if (!rs && !rs1) {
@@ -78,11 +87,14 @@
 					geolocation.getCurrentPosition(function(r) {
 						_this.center = {lng: parseFloat(r.longitude), lat: parseFloat(r.latitude)};		// 设置center属性值
 						_this.autoLocationPoint = {lng: parseFloat(r.longitude), lat: parseFloat(r.latitude)};		// 自定义覆盖物
+						setTimeout(function() {
+							_this.centerIconPoint = {lng: parseFloat(r.longitude), lat: parseFloat(r.latitude)};		// 地图中心覆盖物
+						}, 500);
 						_this.initLocation = true;
 						_this.$store.dispatch('city', r.address.city);
 
 						// 当定位完成，进行搜索附近位置，如果放在getCurrentPosition外面是不可以的，因为只是异步操作。
-						var geocoder = new BMap.Geocoder();
+						let geocoder = new BMap.Geocoder();
 						geocoder.getLocation(new BMap.Point(_this.center.lng, _this.center.lat), function(rs) {
 							if (rs.surroundingPois.length > 0) {
 								_this.$store.dispatch('setOutset', {title: rs.surroundingPois[0].title, address: rs.surroundingPois[0].address, point: rs.surroundingPois[0].point});
@@ -93,7 +105,6 @@
 							console.log('定位附近的地点',rs.surroundingPois)
 						})
 					}, {enableHighAccuracy: true})
-					
 				};
 				if (rs1) {
 					// 搜索起（终）点返回首页时，自动定位到起点
@@ -104,12 +115,14 @@
 						tmp1 = window.localStorage.getItem('Outset');
 					}
 					if (tmp1 != null) {
-						_this.center = {lng: tmp1.point.lng, lat: tmp1.point.lat};
 						// _this.center = {lng: 110.396533, lat: 21.197716};
-						let p1 = new BMap.Point(tmp1.point.lng, tmp1.point.lat)
-						console.log(p1);
-						map.centerAndZoom(p1, 8)
-						console.log('bbbbbbbb');
+						var p1 = new BMap.Point(tmp1.point.lng, tmp1.point.lat);
+						console.log(p1)
+						setTimeout(function() {
+							map.panTo(p1);
+							_this.centerIconPoint = {lng: tmp1.point.lng, lat: tmp1.point.lat}
+						}, 500)
+						console.log('执行到这来了！');
 					} else {
 						console.log('aaaaaa');
 					}
@@ -129,7 +142,8 @@
 				let _this = this;
 				_this.zoom = 15
 				_this.initLocation = false;
-				_this.$store.dispatch('city', data.addressComponent.city)
+				_this.$store.dispatch('city', data.addressComponent.city);
+				_this.centerIconPoint = {lng: data.point.lng, lat: data.point.lat};
 				var geocoder = new BMap.Geocoder();
 				geocoder.getLocation(new BMap.Point(data.point.lng, data.point.lat), function(rs) {
 					var lbs_point = '';
@@ -173,7 +187,35 @@
 						console.log('失败回调',error);
 					}
 				)
-			}
+			},
+			// 
+			movestart ({type, target}) {
+				let _this = this;
+				_this.$store.dispatch('setOutset', '正在获取上车地点…');
+			},
+			// 地图移动结束时触发此事件
+			moveend ({type, target}) {
+				let _this = this;
+				let rs = this.$route.params.searchStatus;	// 判断是否从搜索地点返回
+				
+				let lng_t = _this.map.getCenter().lng;
+				let lat_t = _this.map.getCenter().lat;
+				_this.centerIconPoint = {lng: lng_t, lat: lat_t}
+				let geocoder = new _this.BMap.Geocoder();
+				geocoder.getLocation(new _this.BMap.Point(lng_t, lat_t), function(rs) {
+					if (rs.surroundingPois.length > 0) {
+						_this.$store.dispatch('setOutset', {title: rs.surroundingPois[0].title, address: rs.surroundingPois[0].address, point: rs.surroundingPois[0].point});
+						window.localStorage.setItem('Outset', JSON.stringify(_this.$store.state.outset));
+						// console.log('执行到这来了')
+					} else {
+						// let lc = {title: '当前位置', address: '未知', point: {lng: lng_t, lat: lat_t}};
+						_this.$store.dispatch('setOutset', {title: '当前位置', address: '未知', point: {lng: lng_t, lat: lat_t}});
+						window.localStorage.setItem('Outset', JSON.stringify({title: '当前位置', address: '未知', point: {lng: lng_t, lat: lat_t}}));
+						// console.log('到这里了已经');
+					}
+				})
+			
+			},
 		}
 	}
 </script>
