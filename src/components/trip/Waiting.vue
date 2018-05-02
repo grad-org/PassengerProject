c1c7a07e113f27cdc86729c3feedc284085a8217
Frankie <1426203851@qq.com>
2018-05-03 02:56:40 +0800

    等待接单

--- src/components/trip/Waiting.vue
+++ src/components/trip/Waiting.vue
@@ -1,15 +1,23 @@
 <template>
 	<div>
+		<mu-appbar title="等待应答" titleClass="AppBar">
+		</mu-appbar>
 		<h1>等待司机接单中…</h1>
 		<mu-raised-button label="取消订单" class="raised-button" secondary @click="cancelTrip"/>
 	</div>
 </template>
 
 <script>
+
+	import SockJS from '../../../static/utils/sockjs.js'
+	import Stomp from 'stompjs'
+
 	export default {
 		data () {
 			return {
-				ls_tripdetail: null
+				ls_tripdetail: null,
+				// 建立连接用
+				stompClient: null,
 			}
 		},
 		created () {
@@ -21,9 +29,10 @@
 				let redirect = decodeURIComponent(this.$route.query.redirect || '/');
 				this.$router.push({path: redirect})
 			}
+			this.checkOrder();
 		},
 		mounted () {
-
+			
 		},
 		methods: {
 			cancelTrip () {
@@ -41,10 +50,39 @@
 				.catch((error) => {
 					console.log(error);
 				})
-			}
+			},
+			checkOrder () {
+				let _this = this;
+				let token = window.localStorage.getItem('Token')
+
+				let socket = new SockJS('http://online-ride-hailing.herokuapp.com/orh');
+				_this.stompClient = Stomp.over(socket);
+
+				// 创建连接
+				_this.stompClient.connect(
+					// headers
+					{'Auth-Token': token},
+					// 连接成功的回调函数
+					function connectCallback (frame) {
+						_this.listenOrderSubscription = _this.stompClient.subscribe('/user/queue/hailingService/tripOrder/acceptance-notification', function (tripOrder) {
+							console.log(tripOrder.body);
+							window.localStorage.setItem('T1', tripOrder.body);
+							_this.$router.push({name: 'Progressing'});
+						})
+					},
+					// 连接失败的回调函数
+					function errorCallback (error) {
+						console.log(error);
+						console.log('失败回调',error);
+					}
+				)
+			},
 		},
 		computed: {
 
+		},
+		watch: {
+			
 		}
 	}
 </script>
@@ -52,6 +90,9 @@
 <style scoped>
 	* {
 		text-align: center;
+	}
+	.AppBar {
+		text-align: center
 	}
 	.raised-button {
 		margin: 12px;
