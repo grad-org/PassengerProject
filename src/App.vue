8ba0539c31ae3408802040bef81173b1c1d8bffa
Frankie <1426203851@qq.com>
2018-05-11 02:45:45 +0800

    测试全局WS连接

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,45 @@
 </template>
 
 <script>
+
+	import SockJS from '../static/utils/sockjs.js'
+	import Stomp from 'stompjs'
+
 	export default {
-		name: 'App'
+		name: 'App',
+		data () {
+			return {
+				// stompClient: null,
+			}
+		},
+		created () {
+			// // 当token不存在时，不进行连接
+			// if (window.localStorage.getItem('Token') != null) {
+				
+			// 	let sock = new SockJS('http://online-ride-hailing.herokuapp.com/orh');
+			// 	this.stompClient = Stomp.over(sock);
+			// 	this.connect();
+			// }
+		},
+		methods: {
+			// onConnected (frame) {
+			// 	// 连接成功回调，进行订阅
+			// 	// console.log('服务器连接成功');
+			// 	this.$store.dispatch('setStompClient', {obj: this.stompClient, status: true});
+			// },
+			// onError (frame) {
+			// 	// 连接失败回调，打印错误
+			// 	console.log('Failed：', frame);
+			// },
+			// connect () {
+			// 	// 进行连接
+			// 	let token = window.localStorage.getItem('Token');
+			// 	this.stompClient.connect({'Auth-Token': token}, this.onConnected, this.onError);
+			// },
+			// onSend () {
+			// 	// 发送消息
+			// }
+		}
 	}
 </script>
 
