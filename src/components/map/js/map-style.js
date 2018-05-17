5662a4b6f99c69d284b3dd3754926284ba6bf2d1
Frankie <1426203851@qq.com>
2018-05-17 20:57:53 +0800

    修改样式

--- src/components/map/js/map-style.js
+++ src/components/map/js/map-style.js
@@ -1,7 +1,7 @@
 
 export default {
 	style: function () {
-		// 自定义主题，顺序：背景、水系、国道、陆地、边界线、建筑物、人造区域、铁路、地铁
+		// 自定义主题，顺序：背景、水系、国道、陆地、边界线、建筑物、人造区域、铁路、地铁、城市主路
 		var styleJson = [
 			{
 				"featureType": "background",
@@ -27,24 +27,24 @@ export default {
 						"visibility": "on"
 				}
 			},
-			// {
-			// 	"featureType": "highway",
-			// 	"elementType": "geometry.stroke",
-			// 	"stylers": {
-			// 			"color": "#ffd966ff",
-			// 			"weight": "0.2",
-			// 			"visibility": "on"
-			// 	}
-			// },
-			// {
-			// 	"featureType": "highway",
-			// 	"elementType": "geometry.fill",
-			// 	"stylers": {
-			// 			"color": "#fff2ccff",
-			// 			"weight": "1.3",
-			// 			"visibility": "on"
-			// 	}
-			// },
+			{
+				"featureType": "highway",
+				"elementType": "geometry.stroke",
+				"stylers": {
+						"color": "#ffd966ff",
+						"weight": "0.2",
+						"visibility": "on"
+				}
+			},
+			{
+				"featureType": "highway",
+				"elementType": "geometry.fill",
+				"stylers": {
+						"color": "#ffd966ff",
+						"weight": "1.3",
+						"visibility": "on"
+				}
+			},
 			{
 				"featureType": "boundary",
 				"elementType": "all",
@@ -83,7 +83,53 @@ export default {
 						"weight": "0.1",
 						"visibility": "on"
 				}
-			}
+			},
+			{
+				"featureType": "arterial",
+				"elementType": "geometry.fill",
+				"stylers": {
+						  "color": "#ffffffff"
+				}
+			},
+			{
+				"featureType": "arterial",
+				"elementType": "geometry.stroke",
+				"stylers": {
+						  "color": "#ccccccff",
+						  "weight": "0.8"
+				}
+			},
+			{
+				"featureType": "local",
+				"elementType": "geometry.fill",
+				"stylers": {
+						  "color": "#ffffffff"
+				}
+			},
+			{
+				"featureType": "local",
+				"elementType": "geometry.stroke",
+				"stylers": {
+						  "color": "#f3f3f3ff",
+				}
+			},
+			// 教育-文本-不显示
+			{
+				"featureType": "educationlabel",
+				"elementType": "labels",
+				"stylers": {
+						  "visibility": "off"
+				}
+	  		},
+			// 教育-文本-不显示
+			{
+				"featureType": "scenicspotslabel",
+				"elementType": "labels",
+				"stylers": {
+						  "visibility": "off"
+				}
+	  		}
+			
 		];
 		return styleJson
 	}
