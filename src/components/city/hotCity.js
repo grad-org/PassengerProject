3b15cba1ed206cbb0b948205471e898392f8d765
Frankie <1426203851@qq.com>
2018-05-17 20:54:00 +0800

    热门城市

---
+++ src/components/city/hotCity.js
@@ -0,0 +1,30 @@
+
+const HotCity = {
+	
+	hotCityData () {
+
+	var hot_city_data = [
+		{
+			"cityId": "4401",
+			"cityName": "广州市",
+			"cityFirstLetter": "G",
+			"provinceCode": "44"
+		},
+		{
+			"cityId": "4403",
+			"cityName": "深圳市",
+			"cityFirstLetter": "S",
+			"provinceCode": "44"
+		},
+		{
+			"cityId": "5101",
+			"cityName": "成都市",
+			"cityFirstLetter": "C",
+			"provinceCode": "51"
+		}
+	];
+	return hot_city_data;
+}
+}
+
+export default HotCity
