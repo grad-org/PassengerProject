d543b9c7868a953c33e3e66352058f5f64333c5c
Frankie <1426203851@qq.com>
2018-05-19 05:12:27 +0800

    优化用户编辑页面

--- src/components/user/Edit.vue
+++ src/components/user/Edit.vue
@@ -1,34 +1,65 @@
 
 <template>
 	<div>
-		<mu-appbar ref="barDiv" title="取消">
+		<!-- <mu-appbar ref="barDiv" title="取消">
 			<mu-icon-button icon="keyboard_arrow_left" slot="left" @click="goBack"/>
 			<mu-flat-button label="完成" slot="right" @click="updateProfiles"/> 
-		</mu-appbar>
-		<div ref="avaterDiv" style="margin-bottom: 12px; text-align: center">
-			<mu-paper class="paper" circle :zDepth="1" >
-				<img :src="avater" style="width: 100%; height: 100%"/>
-			</mu-paper>
-			<span style="font-size: 24px">{{nickname}}</span>
-			<span style="margin-top: 6px; font-size: 18px; color: #666">不爱吃鱼的猫…</span>
+		</mu-appbar> -->
+		<van-nav-bar
+			title="编辑资料"
+			left-text="返回"
+			right-text="完成"
+			left-arrow
+			fixed
+			@click-left="goBack"
+			@click-right="updateProfiles"
+			ref="NavBar"
+		/>
+		<div :style="FixedTop">
+			<div class="block_title">
+				用户头像
+			</div>
+			<div style="padding: 0 15px" class="van-cell-group van-hairline--top-bottom imageDiv">
+				<!-- <van-uploader :after-read="onRead" accept="image/gif, image/jpeg" multiple style="z-index: 3">
+					<mu-paper class="paper" circle :zDepth="1" style="position: relative">
+						<img :src="avater" style="width: 100%; height: 100%; z-index: 1;"/>
+						<div style="background: rgba(0,0,0,0.2); z-index: 2; position: absolute; left: 0; right: 0; top: 0; bottom: 0"><van-icon name="photograph" width="30px" style="position: absolute; left: 0; right: 0; bottom: 31px; font-size: 20px; color: rgba(255,255,255,0.8)"/></div>
+					</mu-paper>
+				</van-uploader> -->
+				<div >
+					<mu-paper class="paper" circle :zDepth="1" style="position: relative; float: left">
+						<img :src="avater" style="width: 100%; height: 100%; z-index: 1;"/>
+						<div style="background: rgba(0,0,0,0.15); z-index: 2; position: absolute; left: 0; right: 0; top: 0; bottom: 0" v-if="unUpload"><van-icon name="photograph" width="30px" style="position: absolute; left: 0; right: 0; bottom: 27px; font-size: 20px; color: rgba(255,255,255,0.8)"/></div>
+						<van-uploader :after-read="onRead" accept="image/gif, image/jpeg, image/png" multiple style="position: absolute; left: 0; right: 0; top: 0; bottom: 0; z-index: 3"></van-uploader>
+					</mu-paper>
+					<div style="height: 98px; line-height: 98px; margin-left: 92px; font-size: 16px; color: rgba(102, 102, 102, 1);" >上传头像</div>
+				</div>
+					<!-- <div>上传头像</div> -->
+			</div>
+			<div class="block_title">
+				基本资料
+			</div>
+			<van-cell-group>
+				<van-field v-model="nickname" label="昵称" type="textarea" :placeholder="nicknamePlaceholder" minlength="3" maxlength="12" rows="1" autosize @blur="checkNicknameBlur" @focus="checkNicknameFocus"/>
+				<div class="van-cell van-cell__title van-hairline" @click="setAge">
+					<div class="van-cell__title">
+						<span class="van-cell__text">年龄</span>
+					</div>
+					<div class="van-cell__value" :style="ageStyle">{{age}}</div>
+				</div>
+				<div class="van-cell van-cell__title van-hairline" @click="setAge">
+					<div class="van-cell__title">
+						<span class="van-cell__text">性别</span>
+					</div>
+					<div class="van-cell__value" :style="ageStyle">{{gender}}</div>
+				</div>
+			</van-cell-group>
+			<!-- 年龄选择器 -->
+			<mu-bottom-sheet :open="bottomSheet" @close="closeBottomSheet">
+				222222222222
+				45555555
+			</mu-bottom-sheet>
 		</div>
-		<div ref="settingsDiv" :style="{height: mapHeight}" style="background: #fff">
-			<mu-list style="padding: 8px 0 0 0" >
-				<mu-sub-header>基本设置</mu-sub-header>
-				<mu-divider/>
-				<mu-list-item title="实名认证" >
-					<mu-avatar icon="folder" slot="leftAvatar"/>
-					<mu-badge content="已认证" primary slot="after"/>
-				</mu-list-item>
-				<mu-divider/>
-				<mu-list-item title="车主认证" >
-					<mu-avatar icon="folder" slot="leftAvatar"/>
-					<mu-badge content="已认证" primary slot="after"/>
-				</mu-list-item>
-				<mu-divider/>
-			</mu-list>
-		</div>
-		
 	</div>
 </template>
 
@@ -47,85 +78,119 @@
 				avater: null,
 				nickname: '',
 				username: '',
+				age: '',
+				gender: '',
+				userImage: null,
 				ls_userinfo: '',
+				unUpload: true,
 
-				// 组件高度控制
-				fullHeight: document.documentElement.clientHeight,
-				barHeight: '',
-				selectHeight: '',
-				mapHeight: ''
+				// 置顶值
+				FixedTop: {
+					marginTop: ''
+				},
+				ageStyle: {
+					// color: 'red !important'
+					// opacity: 0.9,
+					// filter:'alpha(opacity=70)',
+					// color: '#666',
+					// backgroundColor: 'transparent'
+				},
+				
+				bottomSheet: false,
+				nicknamePlaceholder: '起个有个性的名字吧 ^_^'
 			}
 		},
 		created () {
 			// 用户数据直接在本地获取
-			let _this = this;
-			_this.ls_userinfo = JSON.parse(window.localStorage.getItem('UserInfo'))
-			_this.nickname = _this.ls_userinfo.nickname;
-			_this.avater = avater;
-			this.initHeight();
-			this.setMapHeight();
+			this.ls_userinfo = JSON.parse(window.localStorage.getItem('UserInfo'))
+			this.nickname = this.ls_userinfo.nickname;
+			this.age = this.ls_userinfo.age;
+			this.gender = this.ls_userinfo.gender;
+			this.avater = avater;
 		},
 		mounted() {
-			
+			this.FixedTop.marginTop = this.$refs.NavBar.$el.clientHeight + 'px';
 		},
 		methods: {
 			goBack () {
 				this.$router.go(-1);
 			},
-			updateProfiles () {
-				console.log()
-			},
-			initHeight () {
-				let _this = this
-				window.onresize = function () {
-					return ( ()=> {
-						// 浏览器内容可视高度
-						window.fullHeight = document.documentElement.clientHeight;
-						_this.fullHeight = window.fullHeight;
-						_this.selectHeight = _this.$refs.settingsDiv.clientHeight;
-						_this.mapHeight = (_this.fullHeight - _this.barHeight - _this.selectHeight) + 'px';
-					}) ()
+			// 离开输入框检查用户昵称格式
+			checkNicknameBlur () {
+				let reg = /^[\u4e00-\u9fa5a-zA-Z0-9_]{3,12}$/;
+				if (this.nickname == null || this.nickname == undefined || this.nickname == '') {
+				}
+				else if (!reg.test(this.nickname)) {
+					this.nickname = '';
+					this.nicknamePlaceholder = '长度至少为3，且不含特殊符号！'
+				} else {
+					
 				}
 			},
-			setMapHeight () {
-				this.$nextTick (() => {
-					this.barHeight = this.$refs.barDiv.$el.clientHeight;
-					this.selectHeight = this.$refs.settingsDiv.clientHeight;
-					// 页面加载后，对地图高度进行设置
-					let h = this.fullHeight - this.barHeight - this.selectHeight;
-					// console.log('地图高度：' +h);
-					this.mapHeight = h + 'px';
+			// 聚焦输入框
+			checkNicknameFocus () {
+				if (this.nickname == null || this.nickname == undefined || this.nickname == '') {
+					this.nicknamePlaceholder = '起个有个性的名字吧 ^_^'
+				}
+			},
+			updateProfiles () {
+				this.$axios.post('/api/user/6',
+				{
+					'nickname': '越前君噢',
+					'gender': '男',
+					'age': '21',
+					'userImage': this.userImage
+				}).then((response) => {
+					console.log(response);
+				}).catch((error) => {
+					console.log(error);
 				})
 			},
-		},
-		watch: {
-			// 如果 fullHeight 发生改变，这个函数就会运行
-			fullHeight (val) {
-				if(!this.timer) {
-					this.fullHeight = val
-					this.timer = true
-					let that = this
-					setTimeout(function () {
-						that.timer = false
-					}, 1000)
-				}
-				// console.log("触发watch的fullHeight")
+			onRead(file) {
+				console.log(file);
+				// console.log(file.content);
+				this.userImage = file.content;
+				this.avater = file.content;
+				this.unUpload = false;	// 已上传图片，把遮罩层去掉
+			},
+			setAge () {
+				this.ageStyle = {color: '#000'};
+				this.bottomSheet = true;
+			},
+			closeBottomSheet () {
+				this.bottomSheet = false;
 			}
 		}
 	}
 </script>
 
 <style scoped>
-	.paper {
-		display: inline-block;
-		height: 90px;
-		width: 90px;
-		margin: 30px 0 12px 0;
-		text-align: center;
-		overflow: hidden;
+	.block_title {
+		margin: 0;
+		font-weight: 400;
+		font-size: 14px;
+		color: rgba(69, 90, 100, 0.6);
+		padding: 15px 15px 8px 15px;
 	}
 	span {
 		display: block
+	}
+	.van-cell__title {
+		min-width: 90px;
+	}
+	.van-cell__value {
+		text-align: left;
+	}
+	.imageDiv {
+		background: #fff;
+	}
+	.paper {
+		display: inline-block;
+		height: 72px;
+		width: 72px;
+		margin: 12px 0 12px 0;
+		text-align: center;
+		overflow: hidden;
 	}
 </style>
 
