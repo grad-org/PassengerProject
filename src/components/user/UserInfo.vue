014e5b08fe059b2b1ae2d5ab7a022c9b89c3fec5
Frankie <1426203851@qq.com>
2018-05-19 22:58:49 +0800

    修改用户信息

--- src/components/user/UserInfo.vue
+++ src/components/user/UserInfo.vue
@@ -5,41 +5,54 @@
 			<mu-icon-button icon="arrow_back" slot="left" @click="goBack"/>
 			<mu-flat-button label="编辑资料" slot="right" @click="goEdit"/> 
 		</mu-appbar>
-		<div ref="avaterDiv" style="margin-bottom: 12px; text-align: center">
+		<div ref="avaterDiv" style="text-align: center">
 			<mu-paper class="paper" circle :zDepth="1" >
 				<img :src="avater" style="width: 100%; height: 100%"/>
 			</mu-paper>
-			<span style="font-size: 24px">{{nickname}}</span>
-			<span style="margin-top: 6px; font-size: 18px; color: #666">不爱吃鱼的猫…</span>
+			<span style="font-size: 20px; display: block">{{nickname}}</span>
+			<span style="margin: 6px 0 12px 0; display: block; font-size: 14px; color: #666">不爱吃鱼的猫…</span>
 		</div>
-		<div ref="settingsDiv" :style="{height: mapHeight}" style="background: #fff">
-			<mu-list style="padding: 8px 0 0 0" >
-				<mu-sub-header>基本设置</mu-sub-header>
-				<mu-divider/>
-				<mu-list-item title="实名认证" >
-					<mu-avatar icon="folder" slot="leftAvatar"/>
-					<mu-badge content="未认证" slot="after" v-if="true"/>
-					<mu-badge content="已认证" primary slot="after" v-else/>
-				</mu-list-item>
-				<mu-divider/>
-				<mu-list-item title="车主认证" >
-					<mu-avatar icon="folder" slot="leftAvatar"/>
-					<mu-badge content="未认证" slot="after" v-if="isDriver"/>
-					<mu-badge content="已认证" primary slot="after" v-else/>
-				</mu-list-item>
-				<mu-divider/>
-			</mu-list>
+		<div ref="settingsDiv" :style="{height: settingsHeight}" style="background: #fff">
+			<div class="block_title_userinfo">
+				基本设置
+			</div>
+			<!-- 实名认证 -->
+			<div class="van-cell-group van-hairline--top-bottom">
+				<div class="van-cell van-hairline van-cell--clickable">
+					<mu-badge content="已认证" color="#58b99d" style="right: 30px; position: absolute; top: 14px;" v-show="isVerified"/>
+					<div class="van-cell__title">
+						<i class="van-icon van-icon-shop"></i>
+						<span class="van-cell__text">实名认证</span>
+						<!-- <span class="van-tag van-hairline--surround van-tag--danger">标签</span> -->
+						<i class="van-icon van-cell__right-icon van-icon-arrow icon-cover"></i>
+					</div>
+					<div class="van-cell__value van-cell__value--link link-color-cover" v-show="!isVerified">
+						<span>去认证</span>
+					</div>
+				</div>
+			</div>
+			<!-- 车主认证 -->
+			<div class="van-cell-group van-hairline--bottom">
+				<div class="van-cell van-hairline van-cell--clickable">
+					<mu-badge content="已认证" color="#52b4f8" style="right: 30px; position: absolute; top: 14px;" v-show="isDriver"/>
+					<div class="van-cell__title">
+						<i class="van-icon van-icon-shop"></i>
+						<span class="van-cell__text">车主认证</span>
+						<span class="van-tag van-hairline--surround van-tag--danger">认证车主，享更多优惠</span>
+						<i class="van-icon van-cell__right-icon van-icon-arrow icon-cover"></i>
+					</div>
+					<div class="van-cell__value van-cell__value--link link-color-cover" v-show="!isDriver">
+						<span>去申请</span>
+					</div>
+				</div>
+			</div>
 		</div>
-		
 	</div>
 </template>
 
 <script>
 	/**
 	 * 设置背景图片，参考：https://blog.csdn.net/woyidingshijingcheng/article/details/72903800
-	 * 
-	 * 仍需解决问题： 本地获取头像，设置头像
-	 * 如果认证回来，应该重新获取一次用户信息，用来覆盖localstorage信息，否则认证信息可能未更新！
 	 */
 	import avater from '../../assets/image/avater.jpg'
 
@@ -50,12 +63,14 @@
 				nickname: '',
 				username: '',
 				ls_userinfo: '',
+				isDriver: false,	// 是否已认证车主
+				isVerified: false,	// 是否已实名认证
 
 				// 组件高度控制
 				fullHeight: document.documentElement.clientHeight,
 				barHeight: '',
-				selectHeight: '',
-				mapHeight: ''
+				settingsHeight: '',
+				avaterHeight: ''
 			}
 		},
 		created () {
@@ -63,28 +78,25 @@
 			let _this = this;
 			_this.ls_userinfo = JSON.parse(window.localStorage.getItem('UserInfo'))
 			_this.nickname = _this.ls_userinfo.nickname;
-			_this.avater = avater;
-			_this.isDriver();
+			_this.avater = this.$serverUrl + '/images/user/' + this.$store.state.userId + '.jpg';
+			// 根据是否存在driverId，判断是否已认证车主
+			if (_this.ls_userinfo.driverId == null || _this.ls_userinfo.driverId == '' || _this.ls_userinfo.driverId == undefined) {
+				_this.isDriver = false;
+			} else {
+				_this.isDriver = true;
+			}
 			this.initHeight();
-			this.setMapHeight();
+			this.setSetttingsHeight();
 		},
 		mounted() {
 			
 		},
 		methods: {
 			goBack () {
-				this.$router.go(-1);
+				this.$router.push({name: 'Home'});
 			},
 			goEdit () {
 				this.$router.push({name: 'Edit'})
-			},
-			isDriver () {
-				let di = this.ls_userinfo.driverId;
-				if ( di == '' || di == undefined || di == undefined) {
-					return true;
-				} else {
-					return false;
-				}
 			},
 			initHeight () {
 				let _this = this
@@ -93,40 +105,28 @@
 						// 浏览器内容可视高度
 						window.fullHeight = document.documentElement.clientHeight;
 						_this.fullHeight = window.fullHeight;
-						_this.selectHeight = _this.$refs.settingsDiv.clientHeight;
-						_this.mapHeight = (_this.fullHeight - _this.barHeight - _this.selectHeight) + 'px';
+						_this.avaterHeight = _this.$refs.avaterDiv.clientHeight;
+						_this.barHeight = _this.$refs.barDiv.$el.clientHeight;
+						_this.settingsHeight = (_this.fullHeight - _this.barHeight - _this.avaterHeight) + 'px';
+						console.log(_this.settingsHeight)
 					}) ()
 				}
 			},
-			setMapHeight () {
+			setSetttingsHeight () {
 				this.$nextTick (() => {
 					this.barHeight = this.$refs.barDiv.$el.clientHeight;
-					this.selectHeight = this.$refs.settingsDiv.clientHeight;
-					// 页面加载后，对地图高度进行设置
-					let h = this.fullHeight - this.barHeight - this.selectHeight;
-					// console.log('地图高度：' +h);
-					this.mapHeight = h + 'px';
+					this.avaterHeight = this.$refs.avaterDiv.clientHeight;
+					this.settingsHeight = (this.fullHeight - this.barHeight - this.avaterHeight) + 'px';
+					console.log(this.settingsHeight)
 				})
 			},
 		},
-		watch: {
-			// 如果 fullHeight 发生改变，这个函数就会运行
-			fullHeight (val) {
-				if(!this.timer) {
-					this.fullHeight = val
-					this.timer = true
-					let that = this
-					setTimeout(function () {
-						that.timer = false
-					}, 1000)
-				}
-				// console.log("触发watch的fullHeight")
-			}
-		}
+		
 	}
 </script>
 
 <style scoped>
+	@import './css/vant-style.css';
 	.paper {
 		display: inline-block;
 		height: 90px;
@@ -134,9 +134,6 @@
 		margin: 30px 0 12px 0;
 		text-align: center;
 		overflow: hidden;
-	}
-	span {
-		display: block
 	}
 </style>
 
