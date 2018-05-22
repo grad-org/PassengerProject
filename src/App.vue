3c676133563519ee4ddb8b041c911e211dfc5d2d
Frankie <1426203851@qq.com>
2018-05-23 01:59:09 +0800

    新增WebSocket连接成功提示

--- src/App.vue
+++ src/App.vue
@@ -7,43 +7,14 @@
 
 <script>
 
-	import SockJS from '../static/utils/sockjs.js'
-	import Stomp from 'stompjs'
+	import SocketIO from '../static/utils/socket.io.js'
 
 	export default {
 		name: 'App',
-		data () {
-			return {
-				// stompClient: null,
+		sockets: {
+			connect: function () {
+				console.log('WebSocket连接成功了，ID是：', this.$socket.id);
 			}
-		},
-		created () {
-			// // 当token不存在时，不进行连接
-			// if (window.localStorage.getItem('Token') != null) {
-				
-			// 	let sock = new SockJS('http://online-ride-hailing.herokuapp.com/orh');
-			// 	this.stompClient = Stomp.over(sock);
-			// 	this.connect();
-			// }
-		},
-		methods: {
-			// onConnected (frame) {
-			// 	// 连接成功回调，进行订阅
-			// 	// console.log('服务器连接成功');
-			// 	this.$store.dispatch('setStompClient', {obj: this.stompClient, status: true});
-			// },
-			// onError (frame) {
-			// 	// 连接失败回调，打印错误
-			// 	console.log('Failed：', frame);
-			// },
-			// connect () {
-			// 	// 进行连接
-			// 	let token = window.localStorage.getItem('Token');
-			// 	this.stompClient.connect({'Auth-Token': token}, this.onConnected, this.onError);
-			// },
-			// onSend () {
-			// 	// 发送消息
-			// }
 		}
 	}
 </script>
